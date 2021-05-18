b91cc602fa63ecf209d6387dc3073391c14f67c3
trackiss <trackiss@hey.com>
2021-05-18 13:42:34 +0900

    fix 応用01-初級

--- advance/src/test/scala/com/github/trackiss/chapter01/ShokyuSpec.scala
+++ advance/src/test/scala/com/github/trackiss/chapter01/ShokyuSpec.scala
@@ -1,10 +1,13 @@
 package com.github.trackiss.chapter01
 
-import org.scalatest.Assertions
+import Shokyu._
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class ShokyuSpec extends AnyFlatSpec with Assertions {
-  it should "階乗を計算できる" in {
-    assert(Shokyu.fact(5, 1) === 120)
+class ShokyuSpec extends AnyFlatSpec with Matchers:
+  "fact メソッド" should "階乗を計算できる" in {
+    fact(0, 1) shouldBe 1
+    fact(1, 1) shouldBe 1
+    fact(2, 1) shouldBe 2
+    fact(3, 1) shouldBe 6
   }
-}
